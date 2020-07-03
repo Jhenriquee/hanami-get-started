0f24a88e9744c110eef47f8323628763584792dd
= <jriquee@gmail.com>
2020-07-03 13:51:35 -0300

    Adding tests to validate error msgs

--- spec/web/features/add_book_spec.rb
+++ spec/web/features/add_book_spec.rb
@@ -1,6 +1,7 @@
 require 'features_helper'
 
 RSpec.describe 'Add a book' do
+
   after do
     BookRepository.new.clear
   end
@@ -17,5 +18,19 @@ RSpec.describe 'Add a book' do
 
     expect(page).to have_current_path('/books')
     expect(page).to have_content('New book')
+  end
+
+  it 'displays list of errors when params contains errors' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      click_button 'Create'
+    end
+
+    expect(current_path).to eq('/books')
+
+    expect(page).to have_content('There was a problem with your submission')
+    expect(page).to have_content('Title must be filled')
+    expect(page).to have_content('Author must be filled')
   end
 end
